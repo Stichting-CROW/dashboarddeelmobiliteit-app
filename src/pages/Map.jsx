c16aa5e17e7955c96af8c50bb79c1a7e97826664
bartwr <mail@bartroorda.nl>
2021-10-21 12:09:33 +0200

    Made Map show up

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -1,9 +1,30 @@
-// import './App.css';
+import React, { useRef, useEffect, useState } from 'react';
+import maplibregl from 'maplibre-gl';
+
+import './Map.css';
 
 function Map() {
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng] = useState(5.102406);
+  const [lat] = useState(52.0729252);
+  const [zoom] = useState(14);
+
+  // Docs: https://maptiler.zendesk.com/hc/en-us/articles/4405444890897-Display-MapLibre-GL-JS-map-using-React-JS
+  useEffect(() => {
+    if (map.current) return; //stops map from intializing more than once
+    map.current = new maplibregl.Map({
+      container: mapContainer.current,
+      style: 'https://api.maptiler.com/maps/streets/style.json?key=oYg5wHevXnoE2PBNr3iN',
+      center: [lng, lat],
+      zoom: zoom
+    });
+  });
+
   return <div className="Map">
-    The map.
+    <div ref={mapContainer} className="map" />
   </div>
 }
 
 export default Map;
+
