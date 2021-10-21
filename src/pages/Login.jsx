0f6eaf1ca3e570a08dee8cbd572473eceb4db30d
Marc Buma <mosbuma@bumos.nl>
2021-10-21 14:53:01 +0200

    fix recover call

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -71,7 +71,7 @@ const Login = () => {
             />
           
             <div class="flex items-baseline justify-between">
-                <button class="px-6 py-2 mt-4 text-white bg-blue-600 rounded-lg hover:bg-blue-900" onClick={login}>Recover</button>
+                <button class="px-6 py-2 mt-4 text-white bg-blue-600 rounded-lg hover:bg-blue-900" onClick={recover}>Recover</button>
                 <button class="px-6 py-2 mt-4 text-white bg-blue-600 rounded-lg hover:bg-blue-900" onClick={()=>setRecoverPassword(false)}>Cancel</button>
             </div>
           </form>
