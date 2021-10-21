0b08f687c92611fe716fd5a6808d13f40aad9440
Marc Buma <mosbuma@bumos.nl>
2021-10-21 12:09:04 +0200

    added router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import {
+ BrowserRouter as Router,
+ Switch,
+ Route,
+ Link
+} from "react-router-dom";
+import Map from './pages/Map.jsx';
+import Demo from './pages/Demo.jsx';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+         <div>
+           <nav>
+             <ul>
+               <li>
+                 <Link to="/">Home</Link>
+               </li>
+               <li>
+                 <Link to="/map">Map</Link>
+               </li>
+             </ul>
+           </nav>
+
+           <Switch>
+             <Route path="/demo">
+              <Demo />
+             </Route>
+             <Route path="/map">
+              <Map />
+             </Route>
+             <Route path="/">
+              <Demo />
+             </Route>
+           </Switch>
+         </div>
+       </Router>
     </div>
   );
 }
