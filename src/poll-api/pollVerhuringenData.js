c89760be936db142910887fdf13c6062e3486f00
Marc Buma <mosbuma@bumos.nl>
2021-12-07 12:24:58 +0100

    fetch verhuringen (work in progress)

--- src/poll-api/pollVerhuringenData.js
+++ src/poll-api/pollVerhuringenData.js
@@ -37,7 +37,7 @@ const updateVerhuringenData = ()  => {
         }
         
         response.json().then(function(data) {
-          // console.log(data);
+          console.log("update verhuringen data - response json", data)
           // return;
           
           let verhuringen = [];
@@ -49,80 +49,88 @@ const updateVerhuringenData = ()  => {
              "features":[]
           }
       
-          const md5 = require('md5');
+          // const md5 = require('md5');
           // var current_time = moment();
-          verhuringen.forEach(v => {
-            console.log(v);
-            return;
-            // let in_public_space_since = isLoggedIn ? v.start_time : v.in_public_space_since;
-            //
-            // var minutes = current_time.diff(moment(in_public_space_since), 'minutes');
-            // const color = convertDurationToColor(minutes);
+          // verhuringen.start_rentals / verhuringen.end_rentals
+          verhuringen.start_rentals.forEach(v => {
             
-            // {
-            //     "bike_id": "check:e2b733e9-ac8f-459e-9904-19775ff730d7",
-            //     "end_location": {
-            //         "latitude": 51.92392,
-            //         "longitude": 4.46867
-            //     },
-            //     "end_time": "2021-11-01T18:13:01.927898Z",
-            //     "start_location": {
-            //         "latitude": 51.91227,
-            //         "longitude": 4.46921
-            //     },
-            //     "start_time": "2021-11-01T17:52:31.595659Z",
-            //     "system_id": "check",
-            //     "trip_id": 17179722
-            // }
-      
-            let feature = {
-               "type":"Feature",
-               "properties":{
-                  "id":md5(v.start_location.latitude+v.start_location.longitude),
-                  "system_id": v.system_id,
-                  // "in_public_space_since": in_public_space_since,
-                  "color": "#38ff71" // color
-               },
-               "geometry":{
-                  "type":"Point",
-                  "coordinates":[
-                     v.start_location.longitude,
-                     v.start_location.latitude,
-                     0.0
-                  ]
-               }
-            }
-      
-            geoJson.features.push(feature);
-
-            let feature2 = {
-               "type":"Feature",
-               "properties":{
-                  "id":md5(v.end_location.latitude+v.end_location.longitude),
-                  "system_id": v.system_id,
-                  // "in_public_space_since": in_public_space_since,
-                  "color": "#cc0000" // color
-               },
-               "geometry":{
-                  "type":"Point",
-                  "coordinates":[
-                     v.end_location.longitude,
-                     v.end_location.latitude,
-                     0.0
-                  ]
-               }
-            }
-      
-            geoJson.features.push(feature2);
-            // new maplibregl.Marker({color: "#FF0000"})
-            //   .setLngLat([x.location.longitude, x.location.latitude])
-            //   .addTo(map.current);
+            // ***********************************
+            // TODO
+            // ***********************************
+            
+            // // let in_public_space_since = isLoggedIn ? v.start_time : v.in_public_space_since;
+            // //
+            // // var minutes = current_time.diff(moment(in_public_space_since), 'minutes');
+            // // const color = convertDurationToColor(minutes);
             //
-            // return;
+            // // {
+            // //     "bike_id": "check:e2b733e9-ac8f-459e-9904-19775ff730d7",
+            // //     "end_location": {
+            // //         "latitude": 51.92392,
+            // //         "longitude": 4.46867
+            // //     },
+            // //     "end_time": "2021-11-01T18:13:01.927898Z",
+            // //     "start_location": {
+            // //         "latitude": 51.91227,
+            // //         "longitude": 4.46921
+            // //     },
+            // //     "start_time": "2021-11-01T17:52:31.595659Z",
+            // //     "system_id": "check",
+            // //     "trip_id": 17179722
+            // // }
+            //
+            // let feature = {
+            //    "type":"Feature",
+            //    "properties":{
+            //       "id":md5(v.start_location.latitude+v.start_location.longitude),
+            //       "system_id": v.system_id,
+            //       // "in_public_space_since": in_public_space_since,
+            //       "color": "#38ff71" // color
+            //    },
+            //    "geometry":{
+            //       "type":"Point",
+            //       "coordinates":[
+            //          v.start_location.longitude,
+            //          v.start_location.latitude,
+            //          0.0
+            //       ]
+            //    }
+            // }
+            //
+            // geoJson.features.push(feature);
+            //
+            // let feature2 = {
+            //    "type":"Feature",
+            //    "properties":{
+            //       "id":md5(v.end_location.latitude+v.end_location.longitude),
+            //       "system_id": v.system_id,
+            //       // "in_public_space_since": in_public_space_since,
+            //       "color": "#cc0000" // color
+            //    },
+            //    "geometry":{
+            //       "type":"Point",
+            //       "coordinates":[
+            //          v.end_location.longitude,
+            //          v.end_location.latitude,
+            //          0.0
+            //       ]
+            //    }
+            // }
+            //
+            // geoJson.features.push(feature2);
+            // // new maplibregl.Marker({color: "#FF0000"})
+            // //   .setLngLat([x.location.longitude, x.location.latitude])
+            // //   .addTo(map.current);
+            // //
+            // // return;
+            
+            // ***********************************
+            // END TODO
+            // ***********************************
           })
       
           store_verhuringendata.dispatch({
-            type: 'SET_TRIPS',
+            type: 'SET_VERHURINGEN',
             payload: geoJson
           })
         
