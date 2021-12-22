05f630fd69aaf2e73badb13ff1642429bedea3d4
bartwr <mail@bartroorda.nl>
2021-12-22 21:00:56 +0100

    Updated menu design: added icons

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -47,74 +47,110 @@ function Menu() {
 
   return (
     <div className="Menu fixed b-0">
-      <div className="Menu-inner px-4 py-3 flex m-4 mb-1 mx-auto bg-white box-border rounded-3xl w-full shadow-lg">
+      <div className="
+        Menu-inner
+        px-4
+        py-3
+        m-4
+        mb-1
+        mx-auto
+        bg-white
+        box-border
+        rounded-3xl
+        shadow-lg
+        w-full
+        flex
+        flex-col
+        justify-center
+      ">
 
-        {isLoggedIn && <>
-          <Link className={`text-menu ${pathName === '/' || pathName === '/map/park' ? 'is-active' : ''}`} to="/map/park">
-            Aanbod
-          </Link>
+        <div className="flex">
+
+          {isLoggedIn && <>
+            <Link className={`
+              text-menu
+              has-icon
+              icon-aanbod
+              ${pathName === '/' || pathName === '/map/park' ? 'is-active' : ''}
+            `} to="/map/park">
+              Aanbod
+            </Link>
+
+            {/*
+            <Link className={`
+              text-menu
+              has-icon
+              icon-ontwikkeling
+              ${pathName === '/map/trip' ? 'is-active' : ''}
+            `} to="/map/trip">
+              Verhuringen
+            </Link>
+            */}
+
+            <Link className={`
+                text-menu
+                has-icon
+                icon-ontwikkeling
+                ${pathName === '/stats/overview' ? 'is-active' : ''}
+              `} to="/stats/overview">
+              Ontwikkeling
+            </Link>
+          </>}
+
+          {isLoggedIn && false ?
+              <div className="text-menu">
+                <IconButtonFilter  onClick={toggleFilter} />
+              </div>
+              :
+              null }
+
+          {isLoggedIn && false && <Link className={`text-menu
+              ${pathName === '/monitoring' ? 'is-active' : ''}`} to="/monitoring">
+            Monitor
+          </Link>}
+          
+          {isLoggedIn && false && <span>{JSON.stringify(extent)}</span>}
+
+          {isLoggedIn
+            ? <Link className="text-menu" onClick={logOut} to="/">
+                Log uit
+              </Link>
+            : <Link className="text-menu" to="/login">
+                Log in
+              </Link>
+          }
+
+          {isLoggedIn
+            && <a className="text-menu
+              cursor-pointer" href="mailto:info@deelfietsdashboard.nl?subject=Feedback Dashboard Deelmobiliteit&body=Ik heb feedback: ">
+                Feedback 📨
+              </a>}
 
           {/*
-          <Link className={`text-menu ${pathName === '/map/trip' ? 'is-active' : ''}`} to="/map/trip">
-            Verhuringen
-          </Link>
-          */}
-
-          <Link className={`text-menu ${pathName === '/stats/overview' ? 'is-active' : ''}`} to="/stats/overview">
-            Ontwikkeling
-          </Link>
-        </>}
-
-        {isLoggedIn && false ?
-            <div className="text-menu">
-              <IconButtonFilter  onClick={toggleFilter} />
-            </div>
-            :
-            null }
-
-        {isLoggedIn && false && <Link className={`text-menu ${pathName === '/monitoring' ? 'is-active' : ''}`} to="/monitoring">
-          Monitor
-        </Link>}
-        
-        {isLoggedIn && false && <span>{JSON.stringify(extent)}</span>}
-
-        {isLoggedIn
-          ? <Link className="text-menu" onClick={logOut} to="/">
-              Log uit
-            </Link>
-          : <Link className="text-menu" to="/login">
-              Log in
-            </Link>
-        }
-
-        {isLoggedIn
-          && <a className="text-menu cursor-pointer" href="mailto:info@deelfietsdashboard.nl?subject=Feedback Dashboard Deelmobiliteit&body=Ik heb feedback: ">
-              Feedback 📨
-            </a>}
-
-        {/*
-        <Link to="/" className={`text-menu ${pathName === '' ? 'is-active' : ''}`} onClick={(e) => {
-          e.preventDefault();
-          dispatch({
-            type: 'SET_FILTER_DATUM',
-            payload: dateToShow.toISOString()
-          })
-          clearInterval(TO_interval);
-          TO_interval = setInterval(x => {
-            dateToShow.add(30, 'minutes');
+          <Link to="/" className={`text-menu ${pathName === '' ? 'is-active' : ''}`} onClick={(e) => {
+            e.preventDefault();
             dispatch({
               type: 'SET_FILTER_DATUM',
               payload: dateToShow.toISOString()
             })
-          }, 1200);
-          // Stop after 10 minutes
-          setTimeout(x => {
             clearInterval(TO_interval);
-          }, 60 * 1000 * 10);
-        }}>
-          ▶️
-        </Link>
-        */}
+            TO_interval = setInterval(x => {
+              dateToShow.add(30, 'minutes');
+              dispatch({
+                type: 'SET_FILTER_DATUM',
+                payload: dateToShow.toISOString()
+              })
+            }, 1200);
+            // Stop after 10 minutes
+            setTimeout(x => {
+              clearInterval(TO_interval);
+            }, 60 * 1000 * 10);
+          }}>
+            ▶️
+          </Link>
+          */}
+
+        </div>
 
       </div>
     </div>
