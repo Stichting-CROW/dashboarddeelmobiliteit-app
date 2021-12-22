343c89a63a1f13f0883e4a97d21ad50118d0aa68
Sven Boor <sven.boor@gmail.com>
2021-12-22 23:21:45 +0100

    Tweak size of markers.

--- src/components/Map/layers/vehicles-point.js
+++ src/components/Map/layers/vehicles-point.js
@@ -4,7 +4,16 @@ const layer = {
   'source': 'vehicles',
   'layout': {
     'icon-image': ["concat", ['get', 'system_id'], ':', ['get', 'duration_bin']],
-    'icon-size': 1,
+    // 'icon-size': 0.4,
+    'icon-size': [
+      'interpolate',
+        ['linear'],
+        ['zoom'],
+        11,
+        0.4,
+        16,
+        1.0
+      ],
     'icon-allow-overlap': true,
   },
   'minzoom': 7,
