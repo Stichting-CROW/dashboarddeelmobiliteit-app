f73370c14652cd67e5397ec67de275ae83ce243e
bartwr <mail@bartroorda.nl>
2022-01-03 16:30:19 +0100

    Use Markdown to create the About page content #21

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,64 +1,48 @@
 import React from 'react'
+import {marked} from 'marked'
 
 const About = () => {
   return (
-    <div>
-      <h1>
-        Over het Dashboard Deelmobiliteit
-      </h1>
+    <div dangerouslySetInnerHTML={{ __html: marked.parse(`
+# Over het Dashboard Deelmobiliteit
 
-      <h2>
-        Algemeen
-      </h2>
+## Algemeen
 
-      <p>
-        Het Dashboard Deelmobiliteit is een webtool van en voor overheden die de ontwikkelingen rond deelmobiliteit op de voeten willen. Hoe lang en waar staan deelvoertuigen ongebruikt in de openbare ruimte? Hoe vaak worden de deelvoertuigen verhuurd? In welke wijken en op welke tijdstippen zijn deelvoertuigen populair?  Met de informatie uit het Dashboard Deelmobiliteit kunnen overheden hun beleid ontwikkelen, evalueren en bijsturen.
-      </p>
+Het Dashboard Deelmobiliteit is een webtool van en voor overheden die de ontwikkelingen rond deelmobiliteit op de voeten willen. Hoe lang en waar staan deelvoertuigen ongebruikt in de openbare ruimte? Hoe vaak worden de deelvoertuigen verhuurd? In welke wijken en op welke tijdstippen zijn deelvoertuigen populair?  Met de informatie uit het Dashboard Deelmobiliteit kunnen overheden hun beleid ontwikkelen, evalueren en bijsturen.
 
-      <h2>
-        Hoe werkt het?
-      </h2>
+## Hoe werkt het?
 
-      <p>
-        Het Dashboard Deelmobiliteit verzamelt elke halve minuut positie van  meer dan 15.000 geparkeerde deelvoertuigen in Nederland (januari 2022). Op basis deze informatie genereert het dashboard zogenaamde parkeergebeurtenissen en verhuringen, die opgeslagen worden in een centrale database. Het dashboard biedt een aantal functionaliteiten (kaarten, grafieken en tabellen) om de database te raadplegen. Voor analyses waar het dashboard niet in voorziet, kunnen overheden downloads maken van de ruwe data uit het dashboard om maatwerkanalyses te (laten) maken.
-      </p>
+Het Dashboard Deelmobiliteit verzamelt elke halve minuut positie van  meer dan 15.000 geparkeerde deelvoertuigen in Nederland (januari 2022). Op basis deze informatie genereert het dashboard zogenaamde parkeergebeurtenissen en verhuringen, die opgeslagen worden in een centrale database. Het dashboard biedt een aantal functionaliteiten (kaarten, grafieken en tabellen) om de database te raadplegen. Voor analyses waar het dashboard niet in voorziet, kunnen overheden downloads maken van de ruwe data uit het dashboard om maatwerkanalyses te (laten) maken.
 
-      <h2>
-        Functionaliteiten
-      </h2>
-      
-      <p>
-        In overleg met deelnemende overheden zijn de volgende functionaliteiten ontwikkeld:
-      </p>
-      
-      <ul>
-        <li>Een kaart met het aanbod van deelvoertuigen op een tijdstip naar keuze. Een kleurcode geeft tevens aan hoe lang het voertuig te huur staat.</li>
-        <li>Een kaart met verhuringen van deelvoertuigen gedurende een periode naar keuze. Het betreft zowel de locaties waar verhuringen gestart zijn als beëindigd.</li>
-        <li>Grafieken met ontwikkelingen in een periode naar keuze. Het gaat zowel om de ontwikkeling in het aantal deelvoertuigen dat aangeboden wordt als om de ontwikkeling het aantal verhuringen.</li>
-        <li>Standaardrapportages als excel-sheets gedownload kunnen worden.</li>
-      </ul>
+## Functionaliteiten
 
-      <p>
-        Overheden hebben alleen toegang tot de informatie over deelvoertuigen in hun eigen bestuursgebied.
-      </p>
+In overleg met deelnemende overheden zijn de volgende functionaliteiten ontwikkeld:
 
-      <h2>
-        Voordelen voor gemeenten
-      </h2>
+- Een kaart met het aanbod van deelvoertuigen op een tijdstip naar keuze. Een kleurcode geeft tevens aan hoe lang het voertuig te huur staat.
+- Een kaart met verhuringen van deelvoertuigen gedurende een periode naar keuze. Het betreft zowel de locaties waar verhuringen gestart zijn als beëindigd.
+- Grafieken met ontwikkelingen in een periode naar keuze. Het gaat zowel om de ontwikkeling in het aantal deelvoertuigen dat aangeboden wordt als om de ontwikkeling het aantal verhuringen.
+- Standaardrapportages als excel-sheets gedownload kunnen worden.
 
-      <h2>
-        Voordelen aanbieders
-      </h2>
+Overheden hebben alleen toegang tot de informatie over deelvoertuigen in hun eigen bestuursgebied.
 
-      <h2>
-        Tot slot
-      </h2>
+## Voordelen voor gemeenten
 
-      <p>
-        Wil je contact opnemen met het team achter het Dashboard Deelmobiliteit, neem dan contact op met <a href="mailto:info@deelfietsdashboard.nl?Subject=Hallo">info@deelfietsdashboard.nl</a>
-      </p>
+- sneller toegang tot data
+- minder uitvragen naar aanbieders nodig (1 bron met alle data)
+- geen conversie meer nodig: data van elke aanbieder heeft precies hetzelfde dataformaat
+- ontwikkeling in overleg met de aangesloten gemeentes
+- indien gewenst uit te breiden door gemeentes zelf (open source software)
 
-    </div>
+## Voordelen aanbieders
+
+- minder aanvragen vanuit gemeentes
+- geen nieuwe dataformaten nodig i.v.m. gebruik gangbare internationale datastandaarden
+- gemeentes hebben beter inzicht in de stand van zaken
+
+## Tot slot
+
+Wil je contact opnemen met het team achter het Dashboard Deelmobiliteit, neem dan contact op met <a href="mailto:info@deelfietsdashboard.nl?Subject=Hallo">info@deelfietsdashboard.nl</a>
+      `)}} />
   )
 
 }
