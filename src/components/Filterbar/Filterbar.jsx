1cac95b16f8dddc03720c8386fec96abf127a6cd
bartwr <mail@bartroorda.nl>
2022-01-23 22:52:19 +0100

    In public dashboard, show current time. fixes #61

--- src/components/Filterbar/Filterbar.jsx
+++ src/components/Filterbar/Filterbar.jsx
@@ -13,6 +13,7 @@ import {
 import FilteritemHerkomstBestemming from './FilteritemHerkomstBestemming';
 import FilteritemVoertuigTypes from './FilteritemVoertuigTypes.jsx';
 import Logo from '../Logo.jsx';
+import moment from 'moment';
 
 import {
   DISPLAYMODE_PARK,
@@ -24,10 +25,15 @@ function Filterbar({
     displayMode,
     visible,
     hideLogo}) {
+
   const isLoggedIn = useSelector(state => {
     return state.authentication.user_data ? true : false;
   });
-  
+
+  const filterDatum = useSelector(state => {
+    return state.filter && state.filter.datum ? state.filter.datum : new Date().toISOString();
+  });
+
   const ispark=displayMode===DISPLAYMODE_PARK;
   const isrentals=displayMode===DISPLAYMODE_RENTALS;
   const isontwikkeling=displayMode===DISPLAYMODE_OTHER;
@@ -46,6 +52,17 @@ function Filterbar({
       {! hideLogo && <Logo />}
       
       { isLoggedIn && showdatum && <FilteritemDatum /> }
+      
+      { ! isLoggedIn && showdatum && <div>
+        <div className="filter-datum-container">
+          <div className="filter-datum-title">
+            Tijd
+          </div>
+          <div className="filter-datum-box-row">
+            {moment(filterDatum).format('HH:mm')}
+          </div>
+        </div>
+      </div> }
 
       { isLoggedIn && showduur && <FilteritemDuur /> }
 
