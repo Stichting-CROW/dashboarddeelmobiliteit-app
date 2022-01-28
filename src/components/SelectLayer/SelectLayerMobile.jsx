512fb9986be3a846640d632e5f19ea7869d1a541
bartwr <mail@bartroorda.nl>
2022-01-28 18:46:39 +0100

    Fix SelectLayerMobile active state

--- src/components/SelectLayer/SelectLayerMobile.jsx
+++ src/components/SelectLayer/SelectLayerMobile.jsx
@@ -39,11 +39,15 @@ function SelectLayerMobile(props) {
     return state.layers ? state.layers.view_park : DISPLAYMODE_PARKEERDATA_VOERTUIGEN;
   });
 
+  const viewRentals = useSelector(state => {
+    return state.layers ? state.layers.view_rentals : DISPLAYMODE_VERHUURDATA_VOERTUIGEN;
+  });
+
   const isLoggedIn = useSelector(state => {
     return state.authentication.user_data ? true : false;
   });
   
-  // console.log("MOBILE selection", displayMode)
+  console.log("MOBILE selection", displayMode)
   
   const setVisibility = (name, visibility) => {
     dispatch({
@@ -72,12 +76,12 @@ function SelectLayerMobile(props) {
         flex justify-between
       `}>
       { displayMode===DISPLAYMODE_PARK ?
-            <div data-type="heat-map" className={`layer${viewPark!==DISPLAYMODE_PARKEERDATA_HEATMAP ? ' layer-inactive':''}`}
-              onClick={() => { dispatch({ type: 'LAYER_SET_VIEW_PARK', payload: DISPLAYMODE_PARKEERDATA_HEATMAP }) }}>
-              <span className="layer-title">
-                Heat map
-              </span>
-            </div> : null }
+        <div data-type="heat-map" className={`layer${viewPark!==DISPLAYMODE_PARKEERDATA_HEATMAP ? ' layer-inactive':''}`}
+          onClick={() => { dispatch({ type: 'LAYER_SET_VIEW_PARK', payload: DISPLAYMODE_PARKEERDATA_HEATMAP }) }}>
+          <span className="layer-title">
+            Heat map
+          </span>
+        </div> : null }
 
       { displayMode===DISPLAYMODE_PARK ?
         <div data-type="pointers" className={`layer${viewPark!==DISPLAYMODE_PARKEERDATA_CLUSTERS ? ' layer-inactive':''}`}
@@ -85,7 +89,7 @@ function SelectLayerMobile(props) {
           <span className="layer-title">
             Clusters
           </span>
-          </div> : null }
+        </div> : null }
 
       { displayMode===DISPLAYMODE_PARK ?
         <div data-type="vehicles"  className={`layer${viewPark!==DISPLAYMODE_PARKEERDATA_VOERTUIGEN ? ' layer-inactive':''}`}
@@ -93,35 +97,37 @@ function SelectLayerMobile(props) {
           <span className="layer-title">
             Voertuigen
           </span>
-          </div> : null }
+        </div> : null }
 
       { displayMode===DISPLAYMODE_RENTALS ?
-            <div data-type="heat-map" className={`layer${viewPark!==DISPLAYMODE_VERHUURDATA_HEATMAP ? ' layer-inactive':''}`}
-              onClick={() => { dispatch({ type: 'LAYER_SET_VIEW_RENTALS', payload: DISPLAYMODE_VERHUURDATA_HEATMAP }) }}>
-              <span className="layer-title">
-                Heat map
-              </span>
-            </div> : null }
+        <div data-type="heat-map" className={`layer${viewRentals!==DISPLAYMODE_VERHUURDATA_HEATMAP ? ' layer-inactive':''}`}
+          onClick={() => { dispatch({ type: 'LAYER_SET_VIEW_RENTALS', payload: DISPLAYMODE_VERHUURDATA_HEATMAP }) }}>
+          <span className="layer-title">
+            Heat map
+          </span>
+        </div> : null }
 
       { displayMode===DISPLAYMODE_RENTALS ?
-        <div data-type="pointers" className={`layer${viewPark!==DISPLAYMODE_VERHUURDATA_CLUSTERS ? ' layer-inactive':''}`}
+        <div data-type="pointers" className={`layer${viewRentals!==DISPLAYMODE_VERHUURDATA_CLUSTERS ? ' layer-inactive':''}`}
           onClick={() => { dispatch({ type: 'LAYER_SET_VIEW_RENTALS', payload: DISPLAYMODE_VERHUURDATA_CLUSTERS }) }}>
           <span className="layer-title">
             Clusters
           </span>
-          </div> : null }
+        </div> : null
+      }
 
       { displayMode===DISPLAYMODE_RENTALS ?
-        <div data-type="vehicles"  className={`layer${viewPark!==DISPLAYMODE_VERHUURDATA_VOERTUIGEN ? ' layer-inactive':''}`}
+        <div data-type="vehicles"  className={`layer${viewRentals!==DISPLAYMODE_VERHUURDATA_VOERTUIGEN ? ' layer-inactive':''}`}
           onClick={() => { dispatch({ type: 'LAYER_SET_VIEW_RENTALS', payload: DISPLAYMODE_VERHUURDATA_VOERTUIGEN }) }}>
           <span className="layer-title">
             Voertuigen
           </span>
-          </div> : null }
+        </div> : null
+      }
 
       { isLoggedIn && showZoneOnOff ?
           <div data-type="zones" className={`layer${!zonesVisible ? ' layer-inactive':''}`} onClick={() => {
-              dispatch({ type: 'LAYER_TOGGLE_ZONES_VISIBLE', payload: null })
+            dispatch({ type: 'LAYER_TOGGLE_ZONES_VISIBLE', payload: null })
           }}>
             <span className="layer-title">
               Zones
