3858c7b675e465306cbb717cf354a9a32595df40
bartwr <mail@bartroorda.nl>
2022-02-17 09:37:13 +0100

    Updated Tour contents: Added links

--- src/components/Tour/Tour.jsx
+++ src/components/Tour/Tour.jsx
@@ -8,7 +8,7 @@ import { IconButtonClose } from '../IconButtons.jsx';
 
 import './react-slideshow-image.css';
 
-const slideImages = [...Array(12).keys()].slice(1).map(x => {
+const slideImages = [...Array(13).keys()].slice(1).map(x => {
   return `/components/Tour/Openbare_presentatie_dashboard_deelmobiliteit_januari_2022-${x < 10 ? '0' : ''}${x}.png`;
 })
 
@@ -77,7 +77,28 @@ const Tour = () => {
             {slideImages.map((x, idx) => {
               return <div key={x} className="each-slide">
                 <div style={{'backgroundImage': `url(${slideImages[idx]})`}}>
-                  <span></span>
+                  {idx === 10 && <span className="each-slide-text">
+                    <a href="https://docs.crow.nl/deelfietsdashboard/hr-dataspec/" target="_blank" rel="external">
+                      Klik hier voor de specs
+                    </a>
+                  </span>}
+                  {idx === 11 && <span className="each-slide-text">
+                    <div><a href="https://www.fietsberaad.nl/Kennisbank/Afspraken-over-data-en-financiering-van-dashboard" target="_blank" rel="external">
+                      Openbaarheid en financiering
+                    </a></div>
+                    <div><a href="https://github.com/Stichting-CROW/" target="_blank" rel="external">
+                      GitHub
+                    </a></div>
+                    <div><a href="https://gitlab.com/bikedashboard" target="_blank" rel="external">
+                      GitLab
+                    </a></div>
+                    <div><a href="https://github.com/Stichting-CROW/dashboarddeelmobiliteit-app/issues" target="_blank" rel="external">
+                      Bugs & Suggesties
+                    </a></div>
+                    <div><a href="mailto:info@deelfietsdashboard.nl" target="_blank" rel="external">
+                      info@deelfietsdashboard.nl
+                    </a></div>
+                  </span>}
                 </div>
               </div>
             })}
