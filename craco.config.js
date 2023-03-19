5bbb4d6f4852825093481599b3c80eb599688375
bartwr <mail@bartroorda.nl>
2023-03-19 22:48:53 +0100

    Debugging so that production build works

--- craco.config.js
+++ craco.config.js
@@ -10,8 +10,8 @@ module.exports = {
   babel: {
     loaderOptions: {
       ignore: [
-        "./node_modules/mapbox-gl/dist/mapbox-gl.js",
-        "./node_modules/maplibre-gl/dist/maplibre-gl.js"
+        // "./node_modules/mapbox-gl/dist/mapbox-gl.js",
+        // "./node_modules/maplibre-gl/dist/maplibre-gl.js"
       ],
     },
   }
