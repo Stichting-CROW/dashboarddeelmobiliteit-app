cbb30ca1334efd798f0923d199580de39b1a3536
bartwr <mail@bartroorda.nl>
2023-10-25 12:49:37 +0200

    Fix typo: OTHER_GOVERNEMENT -> OTHER_GOVERNMENT

--- src/components/EditOrganisation/EditOrganisation.tsx
+++ src/components/EditOrganisation/EditOrganisation.tsx
@@ -190,7 +190,7 @@ function EditOrganisation({
           >
             <option value=""></option>
             <option value="MUNICIPALITY">Gemeente</option>
-            <option value="OTHER_GOVERNEMENT">Andere overheid</option>
+            <option value="OTHER_GOVERNMENT">Andere overheid</option>
             <option value="OPERATOR">Aanbieder</option>
             <option value="OTHER_COMPANY">Ander bedrijf</option>
             <option value="ADMIN">Admin</option>
