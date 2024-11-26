214a00fd28c6a638bd0e55db85957b391f721075
bartwr <mail@bartroorda.nl>
2024-11-26 16:03:24 +0100

    Fix check on 'is_admin' done when filtering operators

--- src/poll-api/metadataAccessControlList.js
+++ src/poll-api/metadataAccessControlList.js
@@ -93,7 +93,7 @@ export const initAccessControlList = (store_accesscontrollist)  => {
             }
             
             // If not admin, filter out certain operators
-            if(!state.authentication.user_data.acl.is_admin) {
+            if(!state.authentication.user_data.acl || !state.authentication.user_data.acl.is_admin) {
               const hideOperators = ['mywheels', 'greenwheels'];
               metadata.operators = metadata.operators.filter(op => hideOperators.indexOf(op.system_id) <= -1);
             }
