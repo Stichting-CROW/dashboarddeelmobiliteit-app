a1d171986f071b73c84afd996ff28bba92a51268
bartwr <mail@bartroorda.nl>
2024-12-05 15:19:28 +0100

    Update versie -> 2024-12-05

--- src/components/Filterbar/Filterbar.jsx
+++ src/components/Filterbar/Filterbar.jsx
@@ -220,13 +220,13 @@ function Filterbar({
       </div>
     }
 
-    {/* Policy hubs has its own release notes */
-    ! isPolicyHubs &&
-      <div className="absolute top-4 text-xs text-purple-800" style={{left: '110px', fontSize: '0.75rem'}}>
-        versie 2024-11-14<br />
-        - Verbodszone edit werkt beter<br />
-        - Nieuwe GeoPackage-zones import<br />
-      </div>}
+    <div className="absolute text-xs text-purple-800" style={{left: '110px', fontSize: '0.75rem', top: '16px'}}>
+      versie <a href="https://github.com/Stichting-CROW/dashboarddeelmobiliteit-app/blob/main/RELEASES.md#dashboard-deelmobiliteit-app-releases" target='_blank' rel="external" className="underline">
+        2024-12-05
+      </a><br />
+      - Sneller laden parkeerdata<br />
+      - Nieuwe parkeerduurfilters
+    </div>
   </>
 }
 
