b1842fb90214567cd66fd8ca75855a9067f036d8
bartwr <mail@bartroorda.nl>
2025-01-08 23:28:21 +0100

    Add documentation on the Service Areas functionality

--- src/components/Filterbar/Filterbar.jsx
+++ src/components/Filterbar/Filterbar.jsx
@@ -222,9 +222,10 @@ function Filterbar({
 
     <div className="absolute text-xs text-purple-800" style={{left: '110px', fontSize: '0.75rem', top: '16px'}}>
       versie <a href="https://github.com/Stichting-CROW/dashboarddeelmobiliteit-app/blob/main/RELEASES.md#dashboard-deelmobiliteit-app-releases" target='_blank' rel="external" className="underline">
-        2025-01-06
+        2025-01-08
       </a><br />
       - Een goed 2025 gewenst! 💫<br />
+      - <a href="https://dashboarddeelmobiliteit.nl/docs/Servicegebieden/_Introductie.md" className="underline">Servicegebieden</a> docs<br />
     </div>
   </>
 }
