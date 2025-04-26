b68fc56908b60e49290746fc64ad633f1b793572
bartwr <mail@bartroorda.nl>
2025-04-26 23:38:25 +0200

    Code cleanup

--- src/components/PolicyHubsEdit/PolicyHubsEdit_geographyType.tsx
+++ src/components/PolicyHubsEdit/PolicyHubsEdit_geographyType.tsx
@@ -7,65 +7,66 @@ export const PolicyHubsEdit_geographyType = ({
     setHasUnsavedChanges
 }) => {
     const updateGeographyType = (type: string) => {
-        const polygonCenter = hubData?.area ? center(hubData?.area) : null;
+    const polygonCenter = hubData?.area ? center(hubData?.area) : null;
 
-        setHubData({
-            ...hubData,
-            geography_type: type,
-            description: (type === 'no_parking' ? 'Verbodsgebied' : 'Hub'),
-            stop: type === 'stop' ? {
-                ...hubData.stop,
-                is_virtual: hubData.stop?.is_virtual || defaultStopProperties.is_virtual,
-                status: hubData.stop?.status || defaultStopProperties.status,
-                capacity: hubData.stop?.capacity || defaultStopProperties.capacity,
-                location: polygonCenter
-            } : null
-        });
-        setHasUnsavedChanges(true);
-    }
+    setHubData({
+      ...hubData,
+      geography_type: type,
+      description: (type === 'no_parking' ? 'Verbodsgebied' : 'Hub'),
+      stop: type === 'stop' ? {
+        ...hubData.stop,
+        is_virtual: hubData.stop?.is_virtual || defaultStopProperties.is_virtual,
+        status: hubData.stop?.status || defaultStopProperties.status,
+        capacity: hubData.stop?.capacity || defaultStopProperties.capacity,
+        location: polygonCenter
+      } : null
+    });
 
-    return (
-        <div className="
-            mt-0
-            ">
-            <div className="
-                flex
-                rounded-lg bg-white
-                border-solid
-                border
-                border-gray-400
-                text-sm
-            ">
-                {[
-                    {name: 'monitoring', title: 'Analyse', color: '#15aeef'},
-                    {name: 'stop', title: 'Hub', color: '#fd862e'},
-                    {name: 'no_parking', title: 'Verbodsgebied', color: '#fd3e48'}
-                ].map(x => {
-                    return <div className={`
-                        ${hubData.geography_type === x.name ? 'Button-orange' : 'text-gray-500'}
-                        cursor-pointer
-                        flex-1
-                        
-                        rounded-lg
-                        text-center
-                        h-10
-                        flex
-                        flex-col
-                        justify-center
-                    `}
-                    style={{
-                        backgroundColor: `${hubData.geography_type === x.name ? x.color : ''}`,
-                        flex: x.name === 'no_parking' ?  '2' : '1'
-                    }}
-                    key={x.name}
-                    onClick={() => {
-                        updateGeographyType(x.name);
-                    }}
-                    >
-                        {x.title}
-                    </div>
-                })}
-            </div>
-        </div>
-    )
+    setHasUnsavedChanges(true);
+  }
+
+  return (
+    <div className="
+      mt-0
+    ">
+      <div className="
+        flex
+        rounded-lg bg-white
+        border-solid
+        border
+        border-gray-400
+        text-sm
+      ">
+        {[
+          {name: 'monitoring', title: 'Analyse', color: '#15aeef'},
+          {name: 'stop', title: 'Hub', color: '#fd862e'},
+          {name: 'no_parking', title: 'Verbodsgebied', color: '#fd3e48'}
+        ].map(x => {
+          return <div className={`
+            ${hubData.geography_type === x.name ? 'Button-orange' : 'text-gray-500'}
+            cursor-pointer
+            flex-1
+            
+            rounded-lg
+            text-center
+            h-10
+            flex
+            flex-col
+            justify-center
+          `}
+          style={{
+            backgroundColor: `${hubData.geography_type === x.name ? x.color : ''}`,
+            flex: x.name === 'no_parking' ?  '2' : '1'
+          }}
+          key={x.name}
+          onClick={() => {
+            updateGeographyType(x.name);
+          }}
+          >
+            {x.title}
+          </div>
+        })}
+      </div>
+    </div>
+  )
 }
