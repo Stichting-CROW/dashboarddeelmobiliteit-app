5925e0085942af4dcf7fbcabeec496bc9c6d9868
bartwr <mail@bartroorda.nl>
2025-11-28 19:02:24 +0100

    Add "Mechelen" to municipality list for guest users

--- src/components/Filterbar/Filterbar.jsx
+++ src/components/Filterbar/Filterbar.jsx
@@ -254,7 +254,7 @@ function Filterbar({
 
     <div className="absolute text-xs text-purple-800" style={{left: '102px', fontSize: '0.75rem', top: '16px', width: '210px'}}>
       versie <a href="https://github.com/Stichting-CROW/dashboarddeelmobiliteit-app/blob/main/RELEASES.md#dashboard-deelmobiliteit-app-releases" target='_blank' rel="external noreferrer" className="underline">
-        2025-11-27
+        2025-11-28
       </a><br />
       - X-as van grafieken weer correct<br />
       - Aanbod laadt veel sneller<br />
